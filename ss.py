74a4de91d75ace452e2f04ced30101f758b740aa
darekpages <114685277+darekpages@users.noreply.github.com>
2022-09-30 12:39:33 +0200

    Add files via upload

---
+++ ss.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.10
+#-----------------------------------------------------------
+# Script to specify the run or import path
+# modules during prototyping in the console or IDLE.
+# The script must run in the folder where
+# there is another script.
+#
+a= '16.09.2022 (C) DAREKPAGES'
+#-----------------------------------------------------------
+import sys
+
+def pathsys():
+    platf= sys.platform
+    scie= sys.path[0]
+    print('\n'+'System:', platf)
+    print('Launch path:', scie)
+
+print('\t'+'Defining the script run path'+'\n\t'+
+      'Copyright '+a[6:])
+pathsys()
